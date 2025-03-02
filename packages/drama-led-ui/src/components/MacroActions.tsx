7f794712e7970e518320b08a49500dc191e6bd75
jesuitdrama <drama@jhssac.org>
2025-03-02 12:44:54 -0800

    error handling

--- packages/drama-led-ui/src/components/MacroActions.tsx
+++ packages/drama-led-ui/src/components/MacroActions.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useSendMessage } from "@/utils/LEDServerContext";
-import { makeRGBValue } from "@spencer516/drama-led-messages/src/AddressTypes";
 import { useState } from "react";
-import JSONEditor from "./ui/JSONEditor";
 import { InputMessage } from "@spencer516/drama-led-messages/src/InputMessage";
 import { MACRO_EXAMPLES } from "@spencer516/drama-led-messages/src/macros/AllMacros";
+import ErrorModal from "./ui/ErrorModal";
+
+const JSONEditor = dynamic(() => import("./ui/JSONEditor"), { ssr: false });
 
 type Props = {};
 
@@ -26,70 +28,118 @@ function Button({ onClick, label }: ButtonProps) {
   );
 }
 
+function simplifyError(errorObject: unknown): unknown {
+  if (typeof errorObject === "object") {
+    const result: any = {};
+
+    // @ts-ignore
+    for (const [key, value] of Object.entries(errorObject)) {
+      if (key === "_errors") {
+        if (Array.isArray(value) && value.length > 0) {
+          result.errors = value;
+        }
+      } else {
+        result[key] = simplifyError(value);
+      }
+    }
+
+    return result;
+  } else {
+    return errorObject;
+  }
+}
+
 export default function MacroActions({}: Props) {
   const sendMessage = useSendMessage();
   const [code, setCode] = useState("{}");
+  const [error, setError] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const sendWithValidation = () => {
+    try {
+      const result = InputMessage.safeParse(JSON.parse(code));
+
+      if (result.success) {
+        setError(null);
+        sendMessage(result.data);
+      } else {
+        setIsModalOpen(true);
+        setError(JSON.stringify(simplifyError(result.error.format()), null, 2));
+      }
+    } catch (e) {
+      setIsModalOpen(true);
+      setError(String(e));
+    }
+  };
+
   const setCodeObject = (object: InputMessage) => {
     setCode(JSON.stringify(object, null, 2));
   };
   return (
-    <div className="flex gap-4 p-4 box-border">
-      <div className="grow-0 flex flex-col gap-2">
-        <Button
-          label="All On"
-          onClick={() =>
-            setCodeObject({
-              type: "TURN_ALL_ON",
-              data: {},
-            })
-          }
-        />
-        <Button
-          label="All Off"
-          onClick={() =>
-            setCodeObject({
-              type: "TURN_ALL_OFF",
-              data: {},
-            })
-          }
-        />
-        {Object.entries(MACRO_EXAMPLES).map(([label, example]) => (
+    <>
+      <div className="flex gap-4 p-4 box-border">
+        <div className="grow-0 flex flex-col gap-2">
           <Button
-            key={label}
-            label={label}
-            onClick={() => setCodeObject(example)}
+            label="All On"
+            onClick={() =>
+              setCodeObject({
+                type: "TURN_ALL_ON",
+                data: {},
+              })
+            }
           />
-        ))}
+          <Button
+            label="All Off"
+            onClick={() =>
+              setCodeObject({
+                type: "TURN_ALL_OFF",
+                data: {},
+              })
+            }
+          />
+          {Object.entries(MACRO_EXAMPLES).map(([label, example]) => (
+            <Button
+              key={label}
+              label={label}
+              onClick={() => setCodeObject(example)}
+            />
+          ))}
+        </div>
+        <div className="border-slate-300 border grow h-[400px]">
+          <JSONEditor
+            code={code}
+            onChange={setCode}
+          />
+        </div>
+        <div className="flex flex-col grow-0 gap-2 w-[150px]">
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-indigo-500 disabled:bg-gray-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => {
+              sendWithValidation();
+            }}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="rounded-md bg-red-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+            onClick={() => {
+              sendMessage({
+                type: "TURN_ALL_OFF",
+                data: {},
+              });
+            }}
+          >
+            All Off
+          </button>
+        </div>
       </div>
-      <div className="border-slate-300 border grow h-[400px] overflow-auto">
-        <JSONEditor
-          code={code}
-          onChange={setCode}
-        />
-      </div>
-      <div className="flex flex-col grow-0 gap-2 w-[150px]">
-        <button
-          type="button"
-          className="rounded-md bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          onClick={() => {
-            sendMessage(InputMessage.parse(JSON.parse(code)));
-          }}
-        >
-          Submit
-        </button>
-        <button
-          type="button"
-          className="rounded-md bg-red-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-          onClick={() => {
-            sendMessage({
-              type: "TURN_ALL_OFF",
-              data: {},
-            });
-          }}
-        >
-          All Off
-        </button>
-      </div>
-    </div>
+      <ErrorModal
+        isOpen={isModalOpen}
+        setOpen={setIsModalOpen}
+        error={error ?? ""}
+      />
+    </>
   );
 }
--- packages/drama-led-ui/src/components/ui/JSONEditor.tsx
+++ packages/drama-led-ui/src/components/ui/JSONEditor.tsx
@@ -1,76 +1,21 @@
 "use client";
 
 import Editor from "@monaco-editor/react";
-import { useRef } from "react";
-// import { editor, Uri, MarkerSeverity } from "monaco-editor";
-import { InputMessage } from "@spencer516/drama-led-messages/src/InputMessage";
 
 type Props = {
   code: string;
   onChange: (code: string) => void;
 };
 
-type ErrorLocation = {
-  startLineNumber: number;
-  endLineNumber: number;
-  startColumn: number;
-  endColumn: number;
-};
-
-function findErrorLocation(
-  jsonString: string,
-  path: (number | string)[],
-): ErrorLocation {
-  return {
-    startLineNumber: 1,
-    endLineNumber: 1,
-    startColumn: 1,
-    endColumn: 1,
-  };
-}
-
 export default function JSONEditor({ code, onChange }: Props) {
-  // const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
-
-  // const onMount = (editor: editor.IStandaloneCodeEditor) => {
-  //   editorRef.current = editor;
-  // };
-
-  // const validateJSON = (jsonString: string) => {
-  //   try {
-  //     const parsedJson = JSON.parse(jsonString);
-  //     const result = InputMessage.safeParse(parsedJson);
-  //     const currentEditor = editorRef.current;
-
-  //     if (currentEditor == null) {
-  //       return;
-  //     }
-
-  //     // Clear any validation markers
-  //     editor.setModelMarkers(currentEditor.getModel()!, "validation", []);
-
-  //     if (result.success) {
-  //       return;
-  //     }
-
-  //     const markers: editor.IMarker[] = result.error.errors.map((err) => {
-  //       const message = err.message;
-  //       const errorLocation = findErrorLocation(jsonString, err.path);
-
-  //       return {
-  //         severity: MarkerSeverity.Error,
-  //         owner: "",
-  //         resource: Uri.file("/"),
-  //         message,
-  //         ...errorLocation,
-  //       };
-  //     });
-
-  //     editor.setModelMarkers(currentEditor.getModel()!, "validation", markers);
-  //   } catch (e) {
-  //     // Do nothing...Monaco handles JSON errors already.
-  //   }
-  // };
+  // @ts-ignore
+  const onMount = (editor) => {
+    editor.updateOptions({
+      tabSize: 2,
+      selectionClipboard: false,
+      renderValidationDecorations: "on",
+    });
+  };
 
   const handleEditorChange = (code: string | undefined) => {
     if (code == null) {
@@ -78,15 +23,15 @@ export default function JSONEditor({ code, onChange }: Props) {
     }
 
     onChange(code);
-    // validateJSON(code);
   };
 
-  // onMount={onMount}
   return (
     <Editor
       defaultLanguage="json"
       value={code}
       onChange={handleEditorChange}
+      onMount={onMount}
+      options={{}}
     />
   );
 }
